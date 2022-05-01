d5d7f0e23fefc2d2223c954401d51f88b5db5ddb
lluis <lluisgalantexd@gmail.com>
2022-05-01 20:36:35 +0200

    Paralelizado correctamente. Falta Pruebas

--- main.py
+++ main.py
@@ -38,58 +38,77 @@ class MapReduce:
                     word_dict[element] = []
                     for letter in element:
                         word_dict[element].append([letter,1])
-                    mapping_return.append(word_dict)
-                return self.Shuffling(mapping_return)
+                    mapping_return.append([word_dict])
+        #print(mapping_return)
+        return self.Shuffling(mapping_return)
 
-    def Shuffling(self, list_dict_words_letters):
+    def Shuffling(self, lists_dict_words_letters):
         #print('parent process:', os.getppid())
         #print('process id:', os.getpid())
-        for word_dict in list_dict_words_letters:
-            # values = list(word_dict.values())
-            list_key = list(word_dict.keys())
-            key = list_key[0]
+        #print(lists_dict_words_letters)
+        list_shuffling_list=[]
+        for list_dict_words_letters in lists_dict_words_letters:
+            for word_dict in list_dict_words_letters:
+                #print(word_dict)
+                # values = list(word_dict.values())
+                list_key = list(word_dict.keys())
+                key = list_key[0]
+                for value in word_dict.values():
 
-            for value in word_dict.values():
-                non_repeated_dict = dict()
-                for letter_int in value:
-                    for letter in letter_int:
-                        if isinstance(letter, str):
-                            if letter in non_repeated_dict:
-                                non_repeated_dict[letter].append(1)
-                            else:
-                                non_repeated_dict[letter] = [1]
+                    non_repeated_dict = dict()
+                    for letter_int in value:
+                        for letter in letter_int:
+                            if isinstance(letter, str):
+                                if letter in non_repeated_dict:
+                                    non_repeated_dict[letter].append(1)
+                                else:
+                                    non_repeated_dict[letter] = [1]
 
-                word_dict[key] = non_repeated_dict
-                return [word_dict]
+                    word_dict[key] = non_repeated_dict
+                list_shuffling_list.append(word_dict)
+                #print(list_dict_words_letters)
+        #print(list_shuffling_list)
+        return self.Reducing(list_shuffling_list)
 
 
 
 
     def Reducing(self, list_word_letters_shuffled ):
 
+        #print(list_word_letters_shuffled)
         for word_dict in list_word_letters_shuffled:
+            #print(word_dict)
             for value in word_dict.values():
                 #print(value)
                 for letter in value:
                     value[letter] = len(value[letter])
-
+        #print(list_word_letters_shuffled)
+        return list_word_letters_shuffled
 
 
     def printFinalResult(self, list_words_letters_reduced, ficheroFinal):
 
-        sum_Words = len(list_words_letters_reduced) #177
+        sum_Words=0
+        for list in list_words_letters_reduced:
+            sum_Words += len(list)
+
+
+        print(sum_Words)
 
         letters_dictionary = dict()
 
-        for word_dict in list_words_letters_reduced:
-            for value in word_dict.values():
-                # print(value)
-                for letter in value:
-                    if letter in letters_dictionary:
-                        #print(letters_dictionary[letter])
-                        letters_dictionary[letter] = letters_dictionary[letter] + 1
-                    else:
-                        letters_dictionary[letter]=1
+        for list_dict in list_words_letters_reduced:
+
+            for word_dict in list_dict:
+                for value in word_dict.values():
+
+                    # print(value)
+                    for letter in value:
+                        if letter in letters_dictionary:
+                            #print(letters_dictionary[letter])
+                            letters_dictionary[letter] = letters_dictionary[letter] + 1
+                        else:
+                            letters_dictionary[letter]=1
 
         for letter in letters_dictionary:
             numero = (letters_dictionary[letter] / sum_Words) * 100
@@ -109,10 +128,10 @@ f = open(fichero, encoding="UTF-8")
 list_file_lines = f.readlines()  # Reads all the lines and return them as each line a string element in a list
 f.close()
 
-list_file_lines_1000 =[]
+"""list_file_lines_1000 =[]
 for i in range(100000):
     for line in list_file_lines:
-        list_file_lines_1000.append(line)
+        list_file_lines_1000.append(line)"""
 
 
 
@@ -130,16 +149,12 @@ if __name__ == '__main__':
 
     start_time = time.time()
     p = Pool(multiprocessing.cpu_count())
-    shuffling_list = p.map(PruebaMapReduce.Splitting, list_file_lines_1000)
-
-
-
-
-    """for i in range(len(mapping_list)):
-
-        shuffling_list.append(PruebaMapReduce.Shuffling(mapping_list[i]))"""
+    reducing_list = p.map(PruebaMapReduce.Splitting, list_file_lines)
+    p.close()
+    p.join()
+    print(reducing_list)
     end_time = time.time()
-    #print(shuffling_list)
+    PruebaMapReduce.printFinalResult(reducing_list, "Result.txt")
 
     print("Tiempo ejecucion = ",(end_time - start_time))
 
