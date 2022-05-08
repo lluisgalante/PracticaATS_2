beea786d6b503be4932c2beae6c7ccdd6a94a0b4
David Navio <david.navio@zetes.com>
2022-05-08 18:03:51 +0200

    corregir secuencial

--- my_app_main_sequential.py
+++ my_app_main_sequential.py
@@ -1,13 +1,15 @@
 import multiprocessing
 import time
 from multiprocessing import Pool
-import argparse
-import sys
 import matplotlib.pyplot as plt
+import sys
+
 
 class MapReduce:
 
-    def Start_Splitting(self, lines):
+    def CleanAndMapFile(self, lines):
+        """ Replace all 'no words' values from a file that we do not want to map reduce.
+         Once our file lines are cleaned those files will be redirected to mapping function """
         list_file_lines_clean = []
         for line in lines:
             line = line.lower()
@@ -26,6 +28,8 @@ class MapReduce:
         return self.Mapping(list_file_lines_clean)
 
     def Mapping(self, lines):
+        """ Receive file lines and create a dictionary that records the number of times a same letter
+        is repeated on different words of the file"""
         word_dict = dict()
         for line in lines:
             for word in line.split():
@@ -33,118 +37,144 @@ class MapReduce:
                     if letter not in word_dict:
                         word_dict[letter] = 1
                     else:
-                        word_dict[letter] += 1
-        return word_dict
+                        word_dict[letter] += 1  # If already exists on dictionary we use existent key
+            return word_dict
 
     def Shuffling(self, list_dict_letters):
-        # SECUENCIAL
-        dict_total = dict()
+        """ Once the list is mapped this function joins all letters with same 'key' and his values in one key """
+        dict_shuffled = dict()
         for letter in list_dict_letters:
-            if letter not in dict_total:
-                dict_total[letter] = [list_dict_letters[letter]]
+            if letter not in dict_shuffled:
+                dict_shuffled[letter] = [list_dict_letters[letter]]
             else:
-                dict_total[letter].append(list_dict_letters[letter])
-        return dict_total
+                dict_shuffled[letter].append(list_dict_letters[letter])
+        return dict_shuffled
 
-    def Reducing(self,  shuffled_dict):
-        # SECUENCIAL
+    def Reducing(self, shuffled_dict):
+        """ Sum all values from the same key. This dict allows to know all the times a letter on file words """
         for letter in shuffled_dict:
-            shuffled_dict[letter]= sum(shuffled_dict[letter])
+            shuffled_dict[letter] = sum(shuffled_dict[letter])
         return shuffled_dict
 
-#----------------------------------------------------------------------
-def WordCounter(input_file_lines):
 
-    total_words = 0
-    for line in input_file_lines:
-        total_words += len(line.split())
-    return total_words
+class DataManager:
 
-def ReadFile(file_name):
-    f = open(file_name, encoding="UTF-8")
-    file_lines = f.readlines()  # Reads all the lines and return them as each line a string element in a list
-    f.close()
-    return file_lines
+    def __init__(self):
+        self.final_result = list()  # List were the final result will be saved
 
-def GenerateResultAndHistogram(total_words,reduced_dict, file_name):
-    result = []
-    histogramX = []
-    histogramY = []
-    iterator = 0
-    for value, key in reduced_dict.items():
-        if iterator == 0:
-            result.append(file_name)
-        else:
-            histogramX.append(value)
-            numero = (key / total_words) * 100
-            histogramY.append(numero)
-            string_numero = str(round(numero, 2)) + "%"
-            result.append('%s : %s' % (value, string_numero))
-        iterator += 1
-    print("File name:", file_name)
-    print("Num words:", total_words)
-    #GenerateHistogram(histogramX, histogramY)
-    return result
+    def ReadFile(self, file_name):
+        """ From a given file reads all the lines and return them as each line a string element in a list """
+        f = open(file_name, encoding="UTF-8")
+        file_lines = f.readlines()
+        f.close()
+        return file_lines
+
+    def WordCounter(self, file_lines):
+        """ Counter of the words inside a single file. This will allow us
+         to eventually generate the percentage of a value """
+        total_words = 0
+        for line in file_lines:
+            total_words += len(line.split())
+        return total_words
+
+    def GenerateResult(self, total_words, reduced_dict, file_name):
+        """ Generate result of a given map reduced dict. Key represents the letter and value
+        the times that this letter appears on words of the file"""
+        result = list()
+        iterator = 0
+        for value, key in reduced_dict.items():
+            if iterator == 0:
+                result.append(file_name)  # First we save the name of the file
+            else:
+                number = (key / total_words) * 100
+                percentage_number = str(round(number, 2)) + "%"  # Convert number of times to percentage
+                result.append('%s : %s' % (value, percentage_number))
+            iterator += 1
+        print("Num words:", total_words)
+        self.final_result.append(result)
+
+    def PrintAndWriteFileResult(self, destination_file):
+        """ Print data result on screen and write this data in a result file """
+        with open(destination_file, 'w', encoding="UTF-8") as f:
+            for file_result in self.final_result:
+                for data_result in file_result:
+                    print(data_result)
+                    f.write('%s\n' % data_result)
 
 
-def GenerateHistogram(histogramX, histogramY):
+class HistogramGenerator:
+    def __init__(self):
+        self.histograms_list = list()  # List to store all histograms data
 
-    plt.bar(histogramX, histogramY, align='center')  # A bar chart
-    plt.xlabel('Letters')
-    plt.ylabel('Frequency(%)')
+    def GenerateHistogramData(self, total_words, reduced_dict):
+        """ Generate histogram data from each file.
+        This data of each histogram will be appended to class histograms_list"""
+        histogram_x = list()
+        histogram_y = list()
+        histogram_xy = list()
+        for value, key in reduced_dict.items():
+            histogram_x.append(value)
+            number = (key / total_words) * 100
+            histogram_y.append(number)
+        histogram_xy.append(histogram_x)
+        histogram_xy.append(histogram_y)
+        self.histograms_list.append(histogram_xy)
+
+    def GenerateHistogram(self):
+        """ Generate a Histogram based on 'n' files """
+        legends = 'File n'
+        for i in range(0, len(self.histograms_list)):
+            plt.bar(self.histograms_list[i][0], self.histograms_list[i][1],
+                    label=legends.replace("n", str(i + 1)))  # Diff colour of each bar will be assigned automatically
+        plt.xlabel('Letters')
+        plt.ylabel('Frequency(%)')
+        plt.legend()
 
 
-def GenerateFile(result_file, destination_file):
-    with open(destination_file, 'w', encoding="UTF-8") as f:
-        for file in result_file:
-            for data in file:
-                print(data)
-                f.write('%s\n' % data)
-#----------------------------------------------------------------------
+def main():
+    # 1 - Create objects of different classes and a list to save file names
+    data = DataManager()
+    histogram = HistogramGenerator()
+    mapReduced = MapReduce()
+    files_name = list()
 
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser()
-    files = []
-    final_result = []
-
-    source = "sourcefileN"
-    for i in range(len(sys.argv)-1):
-        final_source = source.replace("N",str(i+1))
-        parser.add_argument(final_source, help="enter correct file name")
-
-    args = parser.parse_args()
-
-    if (len(sys.argv)-1 == 1):
-        files.append(args.sourcefile1)
-    if (len(sys.argv)-1 == 2):
-        files.append(args.sourcefile1)
-        files.append(args.sourcefile2)
-    if (len(sys.argv) - 1 == 3):
-        files.append(args.sourcefile1)
-        files.append(args.sourcefile2)
-        files.append(args.sourcefile3)
+    # 2 - Manage parameters and program configuration
+    for i in range(1, len(sys.argv)):
+        files_name.append(sys.argv[i])
 
     start_time = time.time()
-    for file in files:
+    # 3 - Map reduce from every file introduced on arguments
+    for file in files_name:
+        input_file_lines = data.ReadFile(file)
+        sum_words = data.WordCounter(input_file_lines)
 
-        input_file_lines = ReadFile(file)
-        sum_words = WordCounter(input_file_lines)
+        # 3.1 Mapping
 
-        MapReduced = MapReduce()
-        mapped_list = MapReduced.Start_Splitting(input_file_lines)
-        del input_file_lines
+        mapped_list = mapReduced.CleanAndMapFile(input_file_lines)
+        del input_file_lines  # Eliminate list in order to clean memory
 
-        shuffled_dict = MapReduced.Shuffling(mapped_list)
-        del mapped_list  # Elimina la variable
+        # 3.2 Shuffling
+        shuffled_dict = mapReduced.Shuffling(mapped_list)
+        del mapped_list  # Eliminate list in order to clean memory
 
-        reduced_list = MapReduced.Reducing(shuffled_dict)
+        # 3.3 Reducing
+        reduced_list = mapReduced.Reducing(shuffled_dict)
+        del shuffled_dict  # Eliminate list in order to clean memory
 
-        final_result.append(GenerateResultAndHistogram(sum_words,reduced_list, file))
-        del shuffled_dict
-
-    GenerateFile(final_result, "Result.txt")
-    plt.show()
-
+        # 3.4 Save and generate data
+        data.GenerateResult(sum_words, reduced_list, file)
+        histogram.GenerateHistogramData(sum_words, reduced_list)
     end_time = time.time()
+
+    # 4- Write result and generate histogram if parameter is 'yes'
+    data.PrintAndWriteFileResult("Result.txt")
+    histogram.GenerateHistogram()
+
     print("Execution time: ", (end_time - start_time))
+    plt.show()  # Show histogram
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:  # First argument is always my_app_main.py and it's required to introduce almost 1 more
+        print("Incorrect number or arguments")
+    else:
+        main()
